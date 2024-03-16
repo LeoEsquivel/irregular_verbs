6fe50a28c3aa3e633cb3a1d68811ef7a30f84580
Daniel Esquivel <danielesquivel00.lded@gmail.com>
2024-03-16 13:17:18 -0600

    Se obtiene la informacion de la tabla

--- irregular_scrapper/src/helpers/irregular_scrapper.rs
+++ irregular_scrapper/src/helpers/irregular_scrapper.rs
@@ -1,5 +1,8 @@
-use scraper::Element;
+use scraper::html::Select;
+use scraper::{selector, Element, ElementRef};
 use scraper::{Html, Selector};
+
+use crate::models::irregular_verb::{self, IrregularVerb};
 
 pub struct IrregularScrapper {
     page: Html,
@@ -20,8 +23,38 @@ impl IrregularScrapper {
         } 
     }
 
-    pub fn get_page(&self) -> &Html {
-        &self.page
+    fn get_table(&self) -> Vec<ElementRef>{
+        let selector = Selector::parse("tbody > tr").unwrap();
+
+        let html_tbody = self.page.select(&selector);
+ 
+        html_tbody.skip(1).collect()
+    }
+
+    fn get_data(&self, tr: ElementRef<'_>) -> IrregularVerb {
+        let selector = Selector::parse("td").unwrap();
+        let mut td_elements = tr.select(&selector);
+        
+        let infinitive     = td_elements.next().unwrap().inner_html();
+        let simple_past    = td_elements.next().unwrap().inner_html();
+        let past_participle= td_elements.next().unwrap().inner_html(); 
+        let eng_example    = td_elements.next().unwrap().inner_html(); 
+        let esp_example    = td_elements.next().unwrap().inner_html();
+
+        let irregular_verb = IrregularVerb::new(infinitive, simple_past, past_participle, eng_example, esp_example);
+        irregular_verb
+    }
+
+    pub fn get_verbs_list(&self) -> Vec<IrregularVerb>  {
+        println!("Get list of verbs");
+        
+        let tbody = self.get_table();
+        let mut verbs_list: Vec<IrregularVerb> = Vec::new();
+        for element in tbody {
+            let irregular_verb = self.get_data(element);
+            verbs_list.push(irregular_verb)
+        }
+        verbs_list
     }
 
 }
--- irregular_scrapper/src/main.rs
+++ irregular_scrapper/src/main.rs
@@ -1,7 +1,5 @@
 mod models;
 mod helpers;
-// use crate::models::irregular_verb::IrregularVerb;
-
 
 use crate::helpers::irregular_scrapper::IrregularScrapper;
 
@@ -9,5 +7,6 @@ fn main() {
     let url = "https://www.berlitz.com/es-us/blog/verbos-irregulares-en-ingles";
     let mut irregular_scrapper = IrregularScrapper::new(url.to_string());
 
-    println!("{:?}", irregular_scrapper.get_page())
+    let verbs_list = irregular_scrapper.get_verbs_list();
+    // println!("{:?}", irregular_scrapper.get_page())
 }
