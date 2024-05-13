3bcd1023cef65bfa62800af84ee8b830eac33832
Daniel Esquivel <danielesquivel00.lded@gmail.com>
2024-05-13 15:02:42 -0600

    Ejemplo en ingles/español

--- interfaz_web/scripts/index.js
+++ interfaz_web/scripts/index.js
@@ -2,7 +2,9 @@ import { get_verbs } from './services/http.services.js';
 import { SlotMachine } from './slot_machine.js';
 
 const d = document;
-const btn_get_verb = d.getElementById('btn_get_verb');
+const btn_get_verb  = d.getElementById('btn_get_verb');
+const btn_example   = d.getElementById('btn_example');
+const tbody_example = d.getElementById("tbody_example")
 
 const slot_machine = new SlotMachine();
 
@@ -13,12 +15,32 @@ btn_get_verb.addEventListener('click', (e) => {
     d.getElementById('infinitive').innerText = infinitive;
     d.getElementById('simple_past').innerText = simple_past;
     d.getElementById('past_participle').innerText = past_participle;
-
+    create_tr(eng_example, esp_example)
+    
 });
-
 
 d.addEventListener('DOMContentLoaded', async () => {
     const verbs_list = await get_verbs();
     slot_machine.setVerbList(verbs_list);
 
 });
+
+
+const create_tr = (eng_example = '', esp_example = '') => {
+    tbody_example.innerHTML = "";
+    
+    const tr = d.createElement("tr");
+
+    const td_eng = d.createElement("td");
+    td_eng.innerText = eng_example;
+
+    const td_esp = d.createElement("td");
+    td_esp.innerText = esp_example;
+
+    tr.appendChild(td_eng);
+    tr.appendChild(td_esp);
+    tbody_example.appendChild(tr);
+    tbody_example.closest("table").style.display = "inline-block";
+
+
+}
