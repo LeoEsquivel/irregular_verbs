6780b0e78600a3ed4b22a2f4e0fe8234fbe64c8e
Daniel Esquivel <danielesquivel00.lded@gmail.com>
2024-11-26 17:21:55 -0600

    Cambios menores en index.js (slot_machine)

--- interfaz_web/scripts/index.js
+++ interfaz_web/scripts/index.js
@@ -1,10 +1,10 @@
 import { get_verbs } from './services/http.services.js';
 import { SlotMachine } from './slot_machine.js';
+
 import { eventManager } from '../einaudi/eventdelegationmanager.js';
 
 
 const d = document;
-const tbody_example = d.getElementById("tbody_example")
 
 const slot_machine = new SlotMachine();
 
@@ -21,22 +21,20 @@ eventManager.addEventListener('#btn_get_verb','click', async (e) => {
     d.getElementById('simple_past').innerText = simple_past;
     d.getElementById('past_participle').innerText = past_participle;
     d.getElementById('tooltip_trans').setAttribute('tooltip', translation);
-    debugger
     create_tr(eng_example, esp_example)
 });
 
 const create_tr = (eng_example = '', esp_example = '') => {
+    const tbody_example = d.getElementById("tbody_example");
+
     tbody_example.innerHTML = "";
+    tbody_example.closest("table").style.display = "table";
     
     const tr = d.createElement("tr");
 
     tr.appendChild(create_td(eng_example))
     tr.appendChild(create_td(esp_example))
-    console.log(tr);
     tbody_example.appendChild(tr);
-    console.log(tbody_example);
-    tbody_example.closest("table").style.display = "";
-    
 }
 
 const create_td = (text_value = '') => {
